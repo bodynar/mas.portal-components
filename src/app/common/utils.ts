e26b3ed1295d3d54fbc32ef910e62684a25b164b
bodynar <bodynar@gmail.com>
2020-10-03 20:49:14 +0300

    add font awesome icons, utils

--- src/app/common/utils.ts
+++ src/app/common/utils.ts
@@ -2,6 +2,12 @@ import { MenuItem } from "../components/menu/menu";
 
 import HelloWorld from '../components/hello-world/hello-world';
 
+export const emptyFn = () => {};
+
+export const isNullOrUndefined = (value: any): boolean => {
+    return value === undefined || value === null;
+}
+
 export const getMenuItems = (): Array<MenuItem> => {
     return [
         {
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/scss/bootstrap.scss';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
 
 import App from './app/components/app/app';
 
