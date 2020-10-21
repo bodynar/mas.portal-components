807801aa54048b59e5a49ca1814d8facbce14065
bodynar <bodynar@gmail.com>
2020-10-22 01:32:48 +0300

    add button component

--- src/app/shared/components/button/button.tsx
+++ src/app/shared/components/button/button.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
 
-export default function Button(): JSX.Element {
-    return (
-        <>
-        </>
-    );
+import generateUid from '../../../common/uid';
+
+type ButtonType = 
+    'Custom' | 'Primary' | 'Secondary' |
+    'Info' | 'Light' | 'Dark' |
+    'Success' | 'Warning' | 'Danger';
+
+export type ButtonProps = {
+    caption: string;
+    buttonType: ButtonType;
+    onButtonClick: () => void;
+    isOutlined?: boolean;
 };
+
+const typeClassNameMap: Map<ButtonType, string> =
+    new Map([
+        ['Custom', ''],
+        ['Primary', 'primary'],
+        ['Secondary', 'secondary'],
+        ['Info', 'info'],
+        ['Light', 'light'],
+        ['Dark', 'dark'],
+        ['Success', 'success'],
+        ['Warning', 'warning'],
+        ['Danger', 'danger'],
+    ]);
+
+export default function Button(props: ButtonProps) {
+    const id: string = generateUid();
+
+    const type: string =
+        typeClassNameMap.get(props.buttonType) || '';
+
+    const className: string =
+        type === ''
+            ? ''
+            : (props.isOutlined || false) ? `btn-outline-${type}`: `btn-${type}`;
+
+    return (
+        <button
+            id={id}
+            className={`btn ${className}`}
+            onClick={props.onButtonClick}
+        >
+            {props.caption}
+        </button>
+    );
+}
