3c87322f0aa31998d5ce978b69a52c2c84c2fabb
bodynar <bodynar@gmail.com>
2020-10-27 00:44:57 +0300

    init comments container component

--- src/app/modules/comments/components/comments/comments.tsx
+++ src/app/modules/comments/components/comments/comments.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-export default function Comments(): JSX.Element {
+import Comment from '../comment/comment';
+
+import { mapCommentsToExtendedModel } from '../../utils';
+import CommentItem, { ExtendedCommentItem } from '../../types';
+
+// TODO:
+// 1. Options: flat \ tree, when flat - display responseTo with ling to #
+// 2. Figure out about response tree level (max deep 5 => then flat)
+
+export type CommentsProps = {
+    comments: Array<CommentItem>;
+    displayComments: 'flat' | 'tree';
+    maxDeepLevel: number;
+    onAddCommentClick: (comment: string, responseTo: string) => void;
+};
+
+type CommentsState = {
+    comments: Array<ExtendedCommentItem>;
+    maxDeepLevel: number;
+    displayComments: 'flat' | 'tree';
+}
+
+export default function Comments(props: CommentsProps): JSX.Element {
+    const [commentsState, setState] = React.useState<CommentsState>({
+        ...props,
+        comments: mapCommentsToExtendedModel(props.comments),
+    });
+
     return (
         <div>
-            
+            {commentsState.comments.map(comment => 
+                <Comment
+                    key={comment.id}
+                    comment={comment}
+                    enableResponseTree={commentsState.displayComments === 'tree'}
+                    maxDeepLevel={commentsState.maxDeepLevel}
+                    onAddCommentClick={props.onAddCommentClick}
+                />    
+            )}
         </div>
     );
 };
