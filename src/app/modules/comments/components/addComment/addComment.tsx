c35a9f378ab3e47f155b76b58903c6174b6b1c2c
bodynar <bodynar@gmail.com>
2020-11-08 13:06:30 +0300

    update addComment

--- src/app/modules/comments/components/addComment/addComment.tsx
+++ src/app/modules/comments/components/addComment/addComment.tsx
@@ -31,7 +31,7 @@ export default function AddComment(props: AddCommentProps): JSX.Element {
         ref: React.useRef(null)
     });
 
-    const onAddCommentClick: () => void = (): void => {
+    const onAddCommentClick = React.useCallback((): void => {
         props.onAddCommentClick(addCommentState.comment);
 
         if (!isNullOrUndefined(addCommentState.ref.current)) {
@@ -43,15 +43,15 @@ export default function AddComment(props: AddCommentProps): JSX.Element {
             comment: '',
             hideMask: false,
         });
-    };
+    }, [addCommentState, props]);
 
-    const onNotExpandedInputClick: () => void = (): void => {
+    const onNotExpandedInputClick = React.useCallback((): void => {
         if (!addCommentState.isOpen) {
             setState({ ...addCommentState, isOpen: true });
         }
-    };
+    }, [addCommentState]);
 
-    const onInputHandler: (event: React.FormEvent<HTMLDivElement>) => void =
+    const onInputHandler = React.useCallback(
         (event: React.FormEvent<HTMLDivElement>): void => {
             const comment: string = event.currentTarget.innerText.trimEnd();
 
@@ -60,9 +60,9 @@ export default function AddComment(props: AddCommentProps): JSX.Element {
                 comment: comment,
                 hideMask: comment.length !== 0
             });
-        };
+        }, [addCommentState]);
 
-    const onPasteHandler: (event: React.ClipboardEvent<HTMLDivElement>) => void =
+    const onPasteHandler = React.useCallback(
         (event: React.ClipboardEvent<HTMLDivElement>): void => {
             const clipboardText: string =
                 event.clipboardData.getData("text/plain")
@@ -73,65 +73,86 @@ export default function AddComment(props: AddCommentProps): JSX.Element {
             if (document.queryCommandSupported('insertText')) {
                 document.execCommand('insertText', false, clipboardText);
             }
-        };
+        }, []);
 
     const btnAddTitle: string =
         props.isResponse === true ? 'Response' : 'Add';
-
-    let componentContent: JSX.Element;
-
-    if (addCommentState.isOpen) {
-        componentContent = (
-            <div className={`add-comment__container${addCommentState.hideMask ? ' add-comment__container--not-empty' : ''}`}>
-                <div className="add-comment__pseudo-input-mask">
-                    <span>Add your comment..</span>
-                </div>
-                <div className="add-comment__pseudo-input-container">
-                    <div
-                        className="add-comment__pseudo-input"
-                        contentEditable="true"
-                        onInput={onInputHandler}
-                        onPaste={onPasteHandler}
-                        ref={element => {
-                            element && element.focus();
-
-                            addCommentState.ref.current = element;
-                        }}
-                    >
-                    </div>
-                    <div className="add-comment__pseudo-input-actions">
-                        {props.onCancelClick ?
-                            <div className="add-comment__cancel-button" onClick={props.onCancelClick}>
-                                <span>Cancel</span>
-                            </div>
-                            : null}
-                        <Button
-                            className="add-comment__add-button"
-                            buttonType={'Primary'}
-                            caption={btnAddTitle}
-                            onButtonClick={() => addCommentState.comment.length > 0 ? onAddCommentClick() : null}
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else {
-        componentContent = (
-            <div className="add-comment__container add-comment__container--is-mask" onClick={onNotExpandedInputClick}>
-                <div className="add-comment__pseudo-input-mask">
-                    <span>Add your comment..</span>
-                </div>
-            </div>
-        );
-    }
 
     return (
         <section
             id={addCommentState.uid}
             className="add-comment"
         >
-            {componentContent}
+            {addCommentState.isOpen
+                ? <AddCommentPseudoInput
+                    hideMask={addCommentState.hideMask}
+                    comment={addCommentState.comment}
+                    btnAddTitle={btnAddTitle}
+                    onAddCommentClick={onAddCommentClick}
+                    onInput={onInputHandler}
+                    onPaste={onPasteHandler}
+                    pseudoInputRef={addCommentState.ref}
+                    onCancelClick={props.onCancelClick}
+                />
+                : <AddCommentMask onClick={onNotExpandedInputClick} />}
         </section>
+    );
+};
+
+const AddCommentMask = (props: { onClick: () => void }): JSX.Element => {
+    return (
+        <div className="add-comment__container add-comment__container--is-mask" onClick={props.onClick}>
+            <div className="add-comment__pseudo-input-mask">
+                <span>Add your comment..</span>
+            </div>
+        </div>
+    );
+};
+
+type AddCommentPseudoInputProps = {
+    hideMask: boolean,
+    comment: string
+    btnAddTitle: string,
+    onAddCommentClick: () => void,
+    onInput: (event: React.FormEvent<HTMLDivElement>) => void,
+    onPaste: (event: React.ClipboardEvent<HTMLDivElement>) => void,
+    pseudoInputRef: React.MutableRefObject<HTMLDivElement | null>,
+    onCancelClick?: () => void,
+};
+
+const AddCommentPseudoInput = (props: AddCommentPseudoInputProps): JSX.Element => {
+    return (
+        <div className={`add-comment__container${props.hideMask ? ' add-comment__container--not-empty' : ''}`}>
+            <div className="add-comment__pseudo-input-mask">
+                <span>Add your comment..</span>
+            </div>
+            <div className="add-comment__pseudo-input-container">
+                <div
+                    className="add-comment__pseudo-input"
+                    contentEditable="true"
+                    onInput={props.onInput}
+                    onPaste={props.onPaste}
+                    ref={element => {
+                        element && element.focus();
+
+                        props.pseudoInputRef.current = element;
+                    }}
+                >
+                </div>
+                <div className="add-comment__pseudo-input-actions">
+                    {props.onCancelClick ?
+                        <div className="add-comment__cancel-button" onClick={props.onCancelClick}>
+                            <span>Cancel</span>
+                        </div>
+                        : null}
+                    <Button
+                        className="add-comment__add-button"
+                        buttonType={'Primary'}
+                        caption={props.btnAddTitle}
+                        onButtonClick={() => props.comment.length > 0 ? props.onAddCommentClick() : null}
+                    />
+                </div>
+            </div>
+        </div>
     );
 };
