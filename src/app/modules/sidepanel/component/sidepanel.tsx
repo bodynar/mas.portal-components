e6bf5f9d0f69e70c5441752d5a5877dfb067528a
Atom <bodynar@gmail.com>
2020-11-15 03:20:37 +0300

    use hooks, components instead functions

--- src/app/modules/sidepanel/component/sidepanel.tsx
+++ src/app/modules/sidepanel/component/sidepanel.tsx
@@ -42,7 +42,6 @@ type SidepanelState = {
 };
 
 // TODO:
-// 1. Scrollbar (many items)
 // 2. Active state (selected item)
 
 export default function SidePanel(props: SidePanelProps): JSX.Element {
@@ -51,7 +50,7 @@ export default function SidePanel(props: SidePanelProps): JSX.Element {
         items: props.items.map(x => ({ ...x, uid: generateUid() }))
     });
 
-    const toggleExpanded: () => void = () => setState({ ...state, expanded: !state.expanded });
+    const toggleExpanded = React.useCallback(() => setState({ ...state, expanded: !state.expanded }), [state]);
 
     const backgroundColor: string =
         `#${backgroundColorMap.get(props.background)}`;
@@ -67,9 +66,16 @@ export default function SidePanel(props: SidePanelProps): JSX.Element {
             <aside className="side-panel__panel" style={{ backgroundColor: backgroundColor, color: fontColor }}>
                 <hr style={{ borderTopColor: fontColor }} />
                 <ul className="side-panel__items">
-                    {state.items.map(item => generateSidePanelItem(item))}
+                    {state.items.map(item =>
+                        <SidePanelItem
+                            item={item}
+                        />
+                    )}
                 </ul>
-                {generateExpander(state.expanded, toggleExpanded)}
+                <Expander
+                    expanded={state.expanded}
+                    onClick={toggleExpanded}
+                />
             </aside>
             <main className="side-panel__content-container">
                 <div className="side-panel__side-content">
@@ -80,31 +86,43 @@ export default function SidePanel(props: SidePanelProps): JSX.Element {
     );
 };
 
-const generateExpander = (expanded: boolean, clickHandler: () => void): JSX.Element => {
-    const content: JSX.Element =
-        expanded
-            ? (<div className="side-panel__expander-menu" onClick={clickHandler}>
-                <span>Collapse</span>
-                <i className="fas fa-angle-double-left" />
-            </div>)
-            : (<div className="side-panel__expander-menu" onClick={clickHandler}>
-                <i className="fas fa-angle-double-right" />
-            </div>);
+const Expander = (props: { expanded: boolean, onClick: () => void }): JSX.Element => {
+    const iconClassName: string =
+        props.expanded ? 'left' : 'right';
 
     return (
-        <div className="side-panel__expander" onClick={clickHandler}>
-            {content}
-        </div>);
+        <div className="side-panel__expander" onClick={props.onClick}>
+            <div className="side-panel__expander-menu">
+                {props.expanded
+                    ? <span>Collapse</span>
+                    : <></>
+                }
+                <i className={`fas fa-angle-double-${iconClassName}`} />
+            </div>
+        </div>
+    );
 };
 
-const generateSidePanelItem = (item: SidePanelItem & { uid: string }): JSX.Element => {
-    const elementClass: string = isNullOrUndefined(item.icon) ? ' side-panel__item--no-icon' : '';
-    const iconClass: string = isNullOrUndefined(item.icon) ? ' icon--empty' : ` fa-${item.icon}`;
+const SidePanelItem = (props: { item: SidePanelItem & { uid: string } }): JSX.Element => {
+    const elementClass: string = isNullOrUndefined(props.item.icon) ? ' side-panel__item--no-icon' : '';
+    const iconClass: string = isNullOrUndefined(props.item.icon) ? ' icon--empty' : ` fa-${props.item.icon}`;
+
+    const dataLetter: string | undefined =
+        isNullOrUndefined(props.item.icon)
+            ? props.item.name.toUpperCase().substr(0, 1) : undefined;
 
     return (
-        <li key={item.uid} data-key={item.uid} className={`side-panel__item${elementClass}`}>
-            <i className={`fas${iconClass}`} data-letter={item.name.toUpperCase().substr(0, 1)} />
-            <span>{item.name}</span>
+        <li
+            key={props.item.uid}
+            className={`side-panel__item${elementClass}`}
+        >
+            <i
+                className={`fas${iconClass}`}
+                data-letter={dataLetter}
+            />
+            <span>
+                {props.item.name}
+            </span>
         </li>
     );
 };
