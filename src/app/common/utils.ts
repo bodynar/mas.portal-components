90cc611db3634afde1c23bfdbf555586866b630e
bodynar <bodynar@gmail.com>
2020-12-12 21:39:06 +0300

    update utils

--- src/app/common/utils.ts
+++ src/app/common/utils.ts
@@ -1,8 +1,16 @@
 export const emptyFn = () => { };
 
-export const isNullOrUndefined = (value: any): value is null | undefined => {
-    return value === undefined || value === null;
-};
+export const isUndefined = (value: any): value is undefined => {
+    return value === undefined;
+}
+
+export const isNull = (value: any): value is null => {
+    return value === null;
+}
+
+export const isNullOrUndefined = (value: any): value is undefined | null => {
+    return isUndefined(value) || isNull(value);
+}
 
 export const isNullOrEmpty = (value?: string): boolean => {
     return isNullOrUndefined(value) || value === '';
