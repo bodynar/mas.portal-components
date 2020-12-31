ce7059f7e1f60f70a8a0ee3da70169506b648df7
Atom <bodynar@gmail.com>
2020-12-31 17:28:20 +0300

    add filtering function

--- src/app/common/utils.ts
+++ src/app/common/utils.ts
@@ -4,8 +4,16 @@ export const isNullOrUndefined = (value: any): value is undefined => {
     return value === undefined || value === null;
 };
 
+export const isNullOrEmpty = (value: string): boolean => {
+    return !isNullOrUndefined(value) && value !== '';
+}
+
 export const has = <TItem>(array: Array<TItem>, item: TItem): boolean => {
     return !isNullOrUndefined(
         array.find(x => x === item)
     );
+};
+
+export const hasIn = <TItem>(searchArray: Array<TItem>, sourceArray: Array<TItem>): boolean => {
+    return sourceArray.some(item => has(searchArray, item));
 };
--- src/app/modules/article/utils.ts
+++ src/app/modules/article/utils.ts
@@ -1,3 +1,5 @@
+import { hasIn, isNullOrEmpty } from "../../common/utils";
+
 import { ArticleItem, TagItem } from "./types";
 
 export const filterArticles = (
@@ -6,5 +8,8 @@ export const filterArticles = (
     activeTags: Array<TagItem>,
     searchQuery: string
 ): Array<ArticleItem> => {
-    return [];
+    return [...items]
+        .filter(x => displayArchieved || x.isArchieved)
+        .filter(x => activeTags.length === 0 || hasIn(activeTags, x.tags))
+        .filter(x => isNullOrEmpty(searchQuery) || x.name.toLowerCase().includes(searchQuery.toLowerCase()));
 };
