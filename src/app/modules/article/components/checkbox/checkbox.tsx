a92036c6a4effd954ef93d3747965d6545a87801
Atom <bodynar@gmail.com>
2021-01-08 22:39:29 +0300

    update checkbox

--- src/app/modules/article/components/checkbox/checkbox.tsx
+++ src/app/modules/article/components/checkbox/checkbox.tsx
@@ -9,12 +9,19 @@ type CheckboxProps = {
 };
 
 export default function Checkbox(props: CheckboxProps): JSX.Element {
+    const onChange = React.useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>): void => {
+            props.onChange(event.target.checked);
+        },
+        [props]);
+
     return (
         <label className="checkbox-component">
             <input
+                className="checkbox-component__input"
                 type="checkbox"
                 checked={props.value}
-                onChange={event => props.onChange(event.target.checked)}
+                onChange={onChange}
             />
             <span className="checkbox-component__mark" />
             <span className="checkbox-component__label">{props.label}</span>
