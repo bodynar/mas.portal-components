71384ca9a77cfba3b1d5a72c92ddfdd01e315d84
Atom <bodynar@gmail.com>
2021-01-11 01:18:03 +0300

    update article list mock data

--- src/app/modules/article/page.tsx
+++ src/app/modules/article/page.tsx
@@ -13,54 +13,35 @@ export default function ArticlesPage(): JSX.Element {
         email: 'test@test.test',
     };
 
-    const mockArticles: Array<ArticleItem> = [
-        {
-            id: generateUid(),
-            name: 'Test article 1',
-            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus reiciendis voluptatem suscipit enim. Blanditiis iure, numquam praesentium repellat culpa aut illum unde eveniet, fuga suscipit neque veritatis non nemo delectus.',
-            category: 'Test',
-            author: mockAuthor,
-            stats: {
-                views: 10,
-                likes: 5,
-                dislikes: 4
-            },
-            postedAt: new Date(),
-            isArchieved: false,
-            tags: []
-        },
-        {
-            id: generateUid(),
-            name: '[Arch] Test article 2',
-            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus reiciendis voluptatem suscipit enim. Blanditiis iure, numquam praesentium repellat culpa aut illum unde eveniet, fuga suscipit neque veritatis non nemo delectus.',
-            category: 'Test',
-            author: mockAuthor,
-            stats: {
-                views: 10,
-                likes: 5,
-                dislikes: 4
-            },
-            postedAt: new Date(),
-            isArchieved: true,
-            tags: []
-        },
-        {
-            id: generateUid(),
-            name: '[Arch] Test article 3',
-            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus reiciendis voluptatem suscipit enim. Blanditiis iure, numquam praesentium repellat culpa aut illum unde eveniet, fuga suscipit neque veritatis non nemo delectus.',
-            category: 'Test',
-            author: mockAuthor,
-            stats: {
-                views: 10,
-                likes: 5,
-                dislikes: 4
-            },
-            postedAt: new Date(),
-            isArchieved: true,
-            tags: []
-        },
-    ];
-
+    const mockArticles: Array<ArticleItem> =
+        new Array(200)
+            .fill({
+                id: generateUid(),
+                name: 'Test article',
+                description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus reiciendis voluptatem suscipit enim. Blanditiis iure, numquam praesentium repellat culpa aut illum unde eveniet, fuga suscipit neque veritatis non nemo delectus.',
+                category: 'Test',
+                author: mockAuthor,
+                stats: {
+                    views: 10,
+                    likes: 5,
+                    dislikes: 4
+                },
+                postedAt: new Date(),
+                isArchieved: false,
+                tags: []
+            } as ArticleItem)
+            .map((item, index) => ({
+                ...item,
+                id: generateUid(),
+                name: `${item.name} ${index}`,
+            } as ArticleItem))
+            .map((item, index) => ({
+                ...item,
+                name: index % 4 === 0
+                    ? `[Arch] ${item.name}`
+                    : item.name,
+                isArchieved: index % 4 === 0
+            } as ArticleItem));
     return (
         <>
             <ArticleList
